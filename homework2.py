b051159b7747740c2ff6688078c8ba17d750420a
cherepanovig <cherepanovig@gmail.com>
2024-07-03 10:40:52 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_count = '12'
+hours_count = '1.5'
+course_name = 'Phyton'
+time_for_work = float(hours_count)/float(homework_count)
+print('Курс: '+course_name+', '+'всего задач: '+homework_count+', '+'затрачено часов: '+hours_count+', '+'среднее время выполнения '+str(time_for_work)+' часа.')
